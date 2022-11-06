adc34e14f0f19f8ea255ff3d24fefea42589e2ad
DESKTOP-26LTFUC\VinhNgo <vinhtrungngo1907@gmail.com>
2022-11-06 21:41:04 +0700

    socket and bitcoin sample

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require("express");
 const app = express();
+const http = require("http").createServer(app);
+const delay = require("delay");
+const io = require("socket.io")(http);
 
-app.get("/",function(req,res){
-	res.send("<h1>Hello CodersX Tokyo</h1>");
-})
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
 
-const port =  3000;
-app.listen(port,function(){
-	console.log("Server started at "+port);
-})
+io.on("connection", (socket) => {
+    console.log("New connection", socket.id);
+    socket.on('on-chat', (data) => {
+        console.log(data);
+        io.emit('user-chat', data)
+    });
+});
+
+const port = 3000;
+http.listen(port, function() {
+    console.log("Server started on port " + port);
+});
+
+async function broadcastBitcoinPrice() {
+    while (true) {
+        const price = Math.random() * 400;
+        io.emit("bitcoin-price", {
+            price: parseFloat(price.toFixed(2))
+        });
+        await delay(400);
+    }
+}
+
+broadcastBitcoinPrice();
